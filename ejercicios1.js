97639280e3f893958b3c2e0d7b4638278122d6c5
Joguisam <neokmgt@gmail.com>
2026-04-24 09:39:25 -0600

    feat: :memo: ejercicicos1
    ejercicicos1

---
+++ ejercicios1.js
@@ -0,0 +1,70 @@
+// datos personales.
+let nombre = "Estiben"
+let edad = 17
+let ciudad = "guatemala"
+
+console.log( "hola, mi nombre es ",nombre, "tengo ", edad, "años y vivo en ", ciudad)
+
+//suma de dos numeros.
+let num1 = 10
+let num2 = 10
+
+console.log(num1 + num2)
+
+// resta, multiplicacion y divison
+console.log(num1 - num2)
+console.log(num1 * num2)
+console.log(num1 / num2)
+
+//promedio de tres notas
+
+let nota1 = 100
+let nota2 = 50
+let nota3 = 10
+
+console.log((nota1 + nota2 + nota3)/3 .toFixed(2))
+
+//conversion de edad a meses
+let meses = 12
+console.log(edad * 12)
+
+//horas, minutos, segundos
+let hora = 4
+console.log("cuatro horas a minutos es igual a: ", hora * 60, "y ", (hora * 60) * 60, "segundos")
+
+//iva 19%
+let producto =(console.log("El uniforme de educacion fisica tiene un presio total de Q 100"))
+let uniforme = 100
+let iva = 0.19
+let sinIva = 100 * 0.19 
+let total = uniforme-sinIva
+console.log("ell uniforme sin el valor del iva tendria un valor total de: ", total)
+
+//area de un rectangulo
+let base = 10
+let altura = 20
+let area = base * altura
+
+console.log("El area de el rectangulo es igual a: ", area)
+
+//perimetro de un rectangulo
+let perimetro = 2 * (base + altura)
+console.log("El perímetro del rectángulo es igual a: ", perimetro)
+
+//concatenacion de strings
+let parte1 = "bienveniodo al mundo de la programacion, "
+let parte2 = "aqui tendras el espacio de llevar tus habilidades a otro nivel"
+console.log(parte1+parte2)
+
+//longitud de una palabra
+let palabra= "roncorrorinco"
+console.log(palabra.length)
+
+//primera y ultima letra
+console.log(palabra [0])
+console.log(palabra[palabra.length-1])
+
+//mayuscula a minuscula
+console.log(palabra.toUpperCase())
+console.log(palabra.toLowerCase())
+
